e83986ad63e0b97dbb9d23ebcc70b68dffb0e140
Peter Alfvin <palfvin@gmail.com>
2013-11-16 17:24:33 -0800

    Initial version with bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Avlats::Application.config.secret_key_base = '06fe13e6950cde1cef45e2eff3990f7ba3e58dd35b5a48cc4e304a38d2ea11302ae41152cb348df74943b2c09b9a73a3341d05aaeac1b7dadfa1ba540345b151'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Avlats::Application.config.session_store :cookie_store, key: '_avlats_session'
